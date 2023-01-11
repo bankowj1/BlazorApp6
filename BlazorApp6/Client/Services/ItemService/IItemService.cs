8f73aaab277a2ceaa6d4fa6ab00dcb5334c24344
productivity <productivity@DESKTOP-JN1AH8E>
2023-01-11 02:32:04 +0100

    working Item stuff frame for other if there will be time

--- BlazorApp6/Client/Services/ItemService/IItemService.cs
+++ BlazorApp6/Client/Services/ItemService/IItemService.cs
@@ -3,10 +3,14 @@
     public interface IItemService
     {
         List<Item> items { get; set; }
-        List<ItemsGroup> itemsGroups { get; set; }
-        List<MaterialsItem> materialItems { get; set; }
 
         Task<Item> GetItemAsync(int id);
+
+        Task UpdateItemAsync(Item item);
+
+        Task CreateItemAsync(Item item);
+
+        Task DeleteItemAsync(int id);
 
         Task GetItemsAsync();
     }
--- BlazorApp6/Client/Services/ItemService/ItemService.cs
+++ BlazorApp6/Client/Services/ItemService/ItemService.cs
@@ -1,19 +1,37 @@
-﻿using System.Net.Http.Json;
+﻿using BlazorApp6.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace BlazorApp6.Client.Services.ItemService
 {
     public class ItemService : IItemService
     {
         private readonly HttpClient _httpClient;
+        private readonly NavigationManager _navigationManager;
 
-        public ItemService(HttpClient httpClient)
+        public ItemService(HttpClient httpClient,NavigationManager navigationManager)
         {
             _httpClient = httpClient;
+            _navigationManager = navigationManager;
         }
 
         public List<Item> items { get; set; } = new List<Item>();
-        public List<ItemsGroup> itemsGroups { get; set; } = new List<ItemsGroup>();
-        public List<MaterialsItem> materialItems { get; set; } = new List<MaterialsItem>();
+
+        public async Task CreateItemAsync(Item item)
+        {
+            var res = await _httpClient.PostAsJsonAsync("api/Items", item);
+            if (res == null)
+                throw new Exception("not posted");
+            _navigationManager.NavigateTo("itemList");
+        }
+
+        public async Task DeleteItemAsync(int id)
+        {
+            var res = await _httpClient.DeleteAsync($"api/Items/{id}");
+            if (res == null)
+                throw new Exception("not deleted");
+            _navigationManager.NavigateTo("itemList");
+        }
 
         public async Task<Item> GetItemAsync(int id)
         {
@@ -30,9 +48,18 @@ namespace BlazorApp6.Client.Services.ItemService
             var res = await _httpClient.GetFromJsonAsync<List<Item>>("api/Items");
             if (res == null)
             {
-                return;
+                throw new Exception("not found");
             }
             items = res;
         }
+
+        public async Task UpdateItemAsync(Item item)
+        {
+            var res = await _httpClient.PutAsJsonAsync($"api/Items/{item.Iditem}", item);
+            if (res == null)
+                throw new Exception("not posted");
+            _navigationManager.NavigateTo("itemList");
+        }
+
     }
 }
