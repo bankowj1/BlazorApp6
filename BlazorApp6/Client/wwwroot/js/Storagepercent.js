a016895c302a66f34484979bdafbad48f91eea90
productivity <productivity@DESKTOP-JN1AH8E>
2023-01-12 03:44:54 +0100

    some work

--- BlazorApp6/Client/wwwroot/js/Storagepercent.js
+++ BlazorApp6/Client/wwwroot/js/Storagepercent.js
@@ -1,5 +1,14 @@
-﻿function getStoragePercent(){
-    navigator.storage.estimate().then((estimate) => {
-        return (estimate.usage / estimate.quota * 100).toFixed(2);
-    });
+﻿async function getStoragePercent() {
+    let estimate;
+    try {
+        if ('storage' in navigator && 'estimate' in navigator.storage) {
+            estimate = await navigator.storage.estimate();
+        } else {
+            throw new Error('Estimating storage usage is not supported in this browser.');
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+    return Number((estimate.usage / estimate.quota * 100).toFixed(2));
 }
