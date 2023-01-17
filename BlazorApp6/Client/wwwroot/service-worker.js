c36cb74108874f7f952e12655546f644bf6ade5f
productivity <productivity@DESKTOP-JN1AH8E>
2023-01-17 17:04:17 +0100

    0 before modification of last project

--- BlazorApp6/Client/wwwroot/service-worker.js
+++ BlazorApp6/Client/wwwroot/service-worker.js
@@ -1,10 +1,9 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
+
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+//self.addEventListener('message',event => event.respondWith())
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
